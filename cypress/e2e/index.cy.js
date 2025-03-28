7027d82af57b98d58c5588c716ec716b9b3fd774
martakunsztowicz <kunsztowicz.m@gmail.com>
2025-03-28 18:08:26 +0100

    cypress test for prowly website

--- cypress/e2e/index.cy.js
+++ cypress/e2e/index.cy.js
@@ -1,32 +1,14 @@
-//describe('Testing Prowly website', () => {
-    //it('Visit page and accepts cookies', () => {
-   // cy.visit("www.prowly.com");
-   // cy.get("#ch2-dialog-description").should("be.visible");
-   //cy.get("#ch2-allow-all-btn").click
-    //cy.get("#ch2-dialog-description").should("not.be.visible");
-   // })
-  //})
+describe('Testing Prowly website', () => { 
 
-
-  it('Visit page and scrolls down', () => {
+  it('Accepts all cookies', () => {
     cy.visit("www.prowly.com");
-    cy.get("Your new favorite PR software").scrollIntoView;
-    cy.get("Your new favorite PR software").should('be.visible');
+    cy.get('button.ch2-btn.ch2-allow-all-btn.ch2-btn-primary').click();
+    cy.get('button.ch2-btn.ch2-allow-all-btn.ch2-btn-primary').should('not.be.visible')
+  });
+
+  it('Open /Product/ menu', () => {
+    cy.visit("www.prowly.com");
+    cy.get('button#\\:R2miimH2\\:').click();
+    cy.get('span.paragraph-5.block.font-sans.font-bold.group-hover\\:text-purple-prowly-darker.group-hover\\:no-underline').should('be.visible');
+  });
   })
-
- //it('Visit page and login with wrong data', () => {
-  //  cy.visit("www.prowly.com");
-	//	cy.get('//*[@id="__main"]/header/div/nav/div/a[2]').click
-
-		//cy.get('@myUsername').clear(); //clears input field //this one uses an alias that was created in line 6
-		//cy.get('#user_login').type('username', { delay: 100 }); //types in username
-
-		//cy.get('#user_password').as('myPassword'); //creates an alias //this one uses an alias that was created in line 6
-
-		//cy.get('@myPassword').clear(); //clears input field
-		//cy.get('#user_password').type('password', { delay: 100 }); //types in password
-
-		//cy.get('[name="submit"]').click(); //taps on the 'submit' button
-		//cy.go(-1); //goes back to the previous page it can also go forward
-		//cy.go('forward'); //goes back to the previous page it can also go forward
-  //})
